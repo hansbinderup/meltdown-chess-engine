5e4934dee9d5436f7aaf4ff4b8f707a66932d9c5
Hans Binderup <hbinderup94@gmail.com>
2025-01-31 11:32:02 +0100

    bitboard: initial bitboard
    This commit adds support for uci commands "position->startpos->moves".
    This is reflected in the bitboard which is primitive binary
    representation of the board.
    
    All pieces are stored as 64bit which represents each square on the
    table.
    
    With this commit, we can give the engine commands to move pieces:
     eg: a2a3->move piece from a2 to a3 which will result in handling the
    piece at bit 8 and move it to bit 16.
    
    Might add more information to this commit at one point.
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

---
+++ src/bit_board.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include "file_logger.h"
+#include <cstdint>
+
+constexpr static inline uint8_t s_firstRow { 0 };
+constexpr static inline uint8_t s_secondRow { 8 };
+constexpr static inline uint8_t s_seventhRow { 48 };
+constexpr static inline uint8_t s_eightRow { 56 };
+
+class BitBoard {
+public:
+    BitBoard(FileLogger& logger)
+        : m_logger(logger)
+    {
+        reset();
+    }
+
+    struct Move {
+        const uint8_t from;
+        const uint8_t to;
+    };
+
+    void reset()
+    {
+        m_whitePawns = { 0xffULL << s_secondRow };
+        m_whiteRooks = { 0x81ULL };
+        m_whiteBishops = { 0x24ULL };
+        m_whiteKnights = { 0x42ULL };
+        m_whiteQueens = { 0x08ULL };
+        m_whiteKing = { 0x10ULL };
+
+        m_blackPawns = { 0xffULL << s_seventhRow };
+        m_blackRooks = { 0x81ULL << s_eightRow };
+        m_blackBishops = { 0x24ULL << s_eightRow };
+        m_blackKnights = { 0x42ULL << s_eightRow };
+        m_blackQueens = { 0x08ULL << s_eightRow };
+        m_blackKing = { 0x10ULL << s_eightRow };
+    }
+
+    void perform_move(const Move& move)
+    {
+        m_logger.log("move: {}->{}", move.from, move.to);
+
+        std::optional<uint64_t*> piece;
+        const auto fromSquare = 1ULL << move.from;
+
+        if (fromSquare & m_whitePawns)
+            piece = &m_whitePawns;
+        else if (fromSquare & m_whiteRooks)
+            piece = &m_whiteRooks;
+        else if (fromSquare & m_whiteBishops)
+            piece = &m_whiteBishops;
+        else if (fromSquare & m_whiteKnights)
+            piece = &m_whiteKnights;
+        else if (fromSquare & m_whiteQueens)
+            piece = &m_whiteQueens;
+        else if (fromSquare & m_whiteKing)
+            piece = &m_whiteKing;
+        else if (fromSquare & m_blackPawns)
+            piece = &m_blackPawns;
+        else if (fromSquare & m_blackRooks)
+            piece = &m_blackRooks;
+        else if (fromSquare & m_blackBishops)
+            piece = &m_blackBishops;
+        else if (fromSquare & m_blackKnights)
+            piece = &m_blackKnights;
+        else if (fromSquare & m_blackQueens)
+            piece = &m_blackQueens;
+        else if (fromSquare & m_blackKing)
+            piece = &m_blackKing;
+
+        if (!piece.has_value()) {
+            m_logger.log("could not find a piece at given position..");
+            return;
+        }
+
+        **piece &= ~(1ULL << move.from);
+        **piece |= (1ULL << move.to);
+    }
+
+    void print_board_debug()
+    {
+        m_logger.log("\nBitboard debug:");
+
+        for (uint8_t row = 8; row > 0; row--) {
+            m_logger << std::format("-{}- ", row);
+
+            for (uint8_t column = 0; column < 8; column++) {
+                uint64_t square = 1ULL << (((row - 1) * 8) + column);
+
+                if (square & m_whitePawns) {
+                    m_logger << "WP ";
+                } else if (square & m_whiteRooks) {
+                    m_logger << "WR ";
+                } else if (square & m_whiteBishops) {
+                    m_logger << "WB ";
+                } else if (square & m_whiteKnights) {
+                    m_logger << "WH ";
+                } else if (square & m_whiteQueens) {
+                    m_logger << "WQ ";
+                } else if (square & m_whiteKing) {
+                    m_logger << "WK ";
+                } else if (square & m_blackPawns) {
+                    m_logger << "BP ";
+                } else if (square & m_blackRooks) {
+                    m_logger << "BR ";
+                } else if (square & m_blackBishops) {
+                    m_logger << "BB ";
+                } else if (square & m_blackKnights) {
+                    m_logger << "BH ";
+                } else if (square & m_blackQueens) {
+                    m_logger << "BQ ";
+                } else if (square & m_blackKing) {
+                    m_logger << "BK ";
+                } else {
+                    m_logger << "## ";
+                }
+            }
+
+            m_logger << "\n";
+        }
+
+        m_logger << "    A  B  C  D  E  F  G  H";
+        m_logger << "\n";
+        m_logger.flush();
+    }
+
+private:
+    uint64_t m_whitePawns;
+    uint64_t m_whiteRooks;
+    uint64_t m_whiteBishops;
+    uint64_t m_whiteKnights;
+    uint64_t m_whiteQueens;
+    uint64_t m_whiteKing;
+
+    uint64_t m_blackPawns;
+    uint64_t m_blackRooks;
+    uint64_t m_blackBishops;
+    uint64_t m_blackKnights;
+    uint64_t m_blackQueens;
+    uint64_t m_blackKing;
+
+    FileLogger& m_logger;
+};
--- src/uci_handler.h
+++ src/uci_handler.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "bit_board.h"
 #include "file_logger.h"
 #include <iostream>
 #include <string_view>
@@ -22,16 +23,34 @@ public:
     }
 
 private:
+    static std::pair<std::string_view, std::string_view> split_sv_by_space(std::string_view sv)
+    {
+        const auto firstSep = sv.find_first_of(' ');
+        const auto first = sv.substr(0, firstSep);
+        const auto second = firstSep == std::string_view::npos
+            ? ""
+            : sv.substr(firstSep + 1);
+
+        return std::make_pair(first, second);
+    }
+
+    static std::optional<std::string_view> sv_next_split(std::string_view& sv)
+    {
+        const auto firstSep = sv.find_first_of(' ');
+
+        if (firstSep == std::string_view::npos) {
+            return std::nullopt;
+        }
+
+        const auto ret = sv.substr(0, firstSep);
+        sv = sv.substr(firstSep + 1);
+
+        return ret;
+    }
+
     static bool processInput(std::string_view input)
     {
-        s_fileLogger.flush();
-
-        const auto commandSep = input.find_first_of(' ');
-        const auto command = input.substr(0, commandSep);
-        const auto args = commandSep == std::string_view::npos
-            ? ""
-            : input.substr(commandSep + 1);
-
+        const auto [command, args] = split_sv_by_space(input);
         s_fileLogger.log("processInput, command: {}, args: {}", command, args);
 
         if (command == "uci") {
@@ -69,9 +88,42 @@ private:
         return true;
     }
 
-    static bool handlePosition(std::string_view args)
+    static std::optional<BitBoard::Move> move_from_input(std::string_view sv)
     {
-        std::ignore = args;
+        if (sv.size() < 4) {
+            return std::nullopt;
+        }
+
+        // quick and dirty way to transform move string to integer values
+        const uint8_t fromIndex = (sv.at(0) - 'a') + (sv.at(1) - '1') * 8;
+        const uint8_t toIndex = (sv.at(2) - 'a') + (sv.at(3) - '1') * 8;
+
+        return BitBoard::Move { fromIndex, toIndex };
+    }
+
+    static bool handlePosition(std::string_view input)
+    {
+        auto [command, args] = split_sv_by_space(input);
+        if (command == "startpos") {
+            s_bitBoard.reset();
+            const auto subCommand = sv_next_split(args);
+
+            if (subCommand == "moves") {
+                while (const auto move = move_from_input(args.substr(0, 4))) {
+                    s_bitBoard.perform_move(move.value());
+
+                    auto spaceSep = args.find(' ');
+                    if (spaceSep == std::string_view::npos) {
+                        break;
+                    }
+
+                    args = args.substr(spaceSep + 1);
+                }
+            }
+        }
+
+        s_bitBoard.print_board_debug();
+
         return true;
     }
 
@@ -85,4 +137,5 @@ private:
 
     static inline bool s_isRunning = false;
     static inline FileLogger s_fileLogger { "/tmp/uci.log" };
+    static inline BitBoard s_bitBoard { s_fileLogger };
 };
