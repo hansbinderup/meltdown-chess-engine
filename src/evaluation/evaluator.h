7ba12d6f39656ece3a811a71c3b79708888a43e5
Hans Binderup <hbinderup94@gmail.com>
2025-02-25 16:10:50 +0100

    evaluator: add time control
    Previously the engine would simply search to the preset depth.
    As games can vary in length I've implemented some sort of time control
    where we based on time inputs will consider how much time to spend and
    try to search for as deep as we can.
    
    This is quite primitive but it's better than before, so let's start with
    that!
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/evaluation/evaluator.h
+++ src/evaluation/evaluator.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "magic_enum/magic_enum.hpp"
 #include "src/engine/move_handling.h"
 #include "src/evaluation/material_scoring.h"
 #include "src/evaluation/move_scoring.h"
@@ -26,10 +25,10 @@ public:
 
     constexpr movement::Move getBestMove(const BitBoard& board, std::optional<uint8_t> depthInput = std::nullopt)
     {
-        m_logger.log("Get best move for {}", magic_enum::enum_name(board.player));
+        const auto startTime = std::chrono::system_clock::now();
+        setupTimeControls(startTime, board);
 
-        const uint8_t depth = std::clamp(board.fullMoves, s_minDepth, s_maxDepth);
-        return scanForBestMove(depthInput.value_or(depth), board);
+        return scanForBestMove(startTime, depthInput.value_or(s_maxSearchDepth), board);
     }
 
     constexpr void printEvaluation(const BitBoard& board, std::optional<uint8_t> depthInput = std::nullopt)
@@ -99,20 +98,45 @@ public:
     {
         m_nodes = 0;
         m_scoring.reset();
+        m_isStopped = false;
 
         m_whiteTime = 0;
         m_blackTime = 0;
         m_movesToGo = 0;
-        m_moveTime = 0;
+        m_moveTime.reset();
         m_whiteMoveInc = 0;
         m_blackMoveInc = 0;
     }
 
 private:
-    constexpr movement::Move scanForBestMove(uint8_t depth, const BitBoard& board)
+    constexpr void setupTimeControls(std::chrono::time_point<std::chrono::system_clock> start, const BitBoard& board)
     {
         using namespace std::chrono;
-        const auto startTime = system_clock::now();
+
+        const auto timeLeft = board.player == Player::White ? milliseconds(m_whiteTime) : milliseconds(m_blackTime);
+        const auto timeInc = board.player == Player::White ? milliseconds(m_whiteMoveInc) : milliseconds(m_blackMoveInc);
+        const auto buffer = timeBuffer(board);
+
+        if (m_moveTime) {
+            m_endTime = start + milliseconds(m_moveTime.value()) - buffer + timeInc;
+        } else if (m_movesToGo) {
+            const auto time = timeLeft / m_movesToGo;
+            m_endTime = start + time - buffer + timeInc;
+        } else {
+            /* TODO: how long should we search if no time controls are set? */
+            const auto time = timeLeft / s_defaultAmountMoves;
+            m_endTime = start + time - buffer + timeInc;
+        }
+
+        /* just to make sure that we actually search something - better to run out of time than to not search any moves.. */
+        if (m_endTime <= start) {
+            m_endTime = start + buffer + timeInc;
+        }
+    };
+
+    constexpr movement::Move scanForBestMove(std::chrono::time_point<std::chrono::system_clock> startTime, uint8_t depth, const BitBoard& board)
+    {
+        using namespace std::chrono;
 
         int16_t alpha = s_minScore;
         int16_t beta = s_maxScore;
@@ -122,6 +146,9 @@ private:
          * https://web.archive.org/web/20070705134903/www.seanet.com/%7Ebrucemo/topics/aspiration.htm
          */
         for (uint8_t d = 1; d <= depth; d++) {
+            if (m_isStopped)
+                break;
+
             m_scoring.pvTable().setIsFollowing(true);
 
             int16_t score = negamax(d, board, alpha, beta);
@@ -153,6 +180,9 @@ private:
 
     constexpr int16_t negamax(uint8_t depth, const BitBoard& board, int16_t alpha = s_minScore, int16_t beta = s_maxScore)
     {
+        using namespace std::chrono;
+        checkIfStopped();
+
         m_scoring.pvTable().updateLength(m_ply);
 
         if (depth == 0) {
@@ -192,6 +222,9 @@ private:
             /* perform search with reduced depth (based on reduction limit) */
             int16_t score = -negamax(depth - 1 - s_nullMoveReduction, boardCopy, -beta, -beta + 1);
 
+            if (m_isStopped)
+                return 0;
+
             if (score >= beta) {
                 return beta;
             }
@@ -214,7 +247,7 @@ private:
             m_ply++;
             legalMoves++;
 
-            const auto startTime = std::chrono::system_clock::now();
+            const auto startTime = system_clock::now();
             int16_t score = 0;
 
             /*
@@ -254,6 +287,10 @@ private:
             printMoveInfo(move, startTime);
 
             m_ply--;
+
+            if (m_isStopped)
+                return 0;
+
             movesSearched++;
 
             if (score >= beta) {
@@ -283,7 +320,8 @@ private:
         return alpha;
     }
 
-    void printMoveInfo(const movement::Move& move, const std::chrono::time_point<std::chrono::system_clock>& startTime)
+    void
+    printMoveInfo(const movement::Move& move, const std::chrono::time_point<std::chrono::system_clock>& startTime)
     {
         using namespace std::chrono;
 
@@ -297,6 +335,9 @@ private:
 
     constexpr int16_t quiesence(const BitBoard& board, int16_t alpha, int16_t beta)
     {
+        using namespace std::chrono;
+        checkIfStopped();
+
         m_nodes++;
 
         const int16_t evaluation = materialScore(board);
@@ -324,12 +365,15 @@ private:
 
             m_ply++;
 
-            const auto startTime = std::chrono::system_clock::now();
+            const auto startTime = system_clock::now();
             const int16_t score = -quiesence(newBoard, -beta, -alpha);
 
             printMoveInfo(move, startTime);
 
             m_ply--;
+
+            if (m_isStopped)
+                return 0;
 
             if (score >= beta)
                 // change to beta for hard cutoff
@@ -350,28 +394,51 @@ private:
         });
     }
 
+    constexpr void checkIfStopped()
+    {
+        /* only check every 2048 nodes */
+        if ((m_nodes & 2047) != 0)
+            return;
+
+        if (std::chrono::system_clock::now() > m_endTime) {
+            m_isStopped = true;
+        }
+    }
+
+    constexpr std::chrono::milliseconds timeBuffer(const BitBoard& board)
+    {
+        using namespace std::chrono_literals;
+
+        if (board.fullMoves < 4) {
+            return 500ms;
+        } else if (board.fullMoves < 6) {
+            return 250ms;
+        } else {
+            return 5ms;
+        }
+    }
+
     FileLogger& m_logger;
 
     uint64_t m_nodes {};
     uint8_t m_ply;
+    bool m_isStopped;
 
     MoveScoring m_scoring {};
 
     uint64_t m_whiteTime {};
     uint64_t m_blackTime {};
     uint64_t m_movesToGo {};
-    uint64_t m_moveTime {};
+    std::optional<uint64_t> m_moveTime {};
     uint64_t m_whiteMoveInc {};
     uint64_t m_blackMoveInc {};
 
     std::chrono::time_point<std::chrono::system_clock> m_endTime;
 
     /* search configs */
-    constexpr static inline uint16_t s_minDepth { 8 };
-    constexpr static inline uint16_t s_maxDepth { 10 };
-
     constexpr static inline uint16_t s_fullDepthMove { 4 };
     constexpr static inline uint16_t s_reductionLimit { 3 };
+    constexpr static inline uint16_t s_defaultAmountMoves { 50 };
 
     constexpr static inline uint8_t s_aspirationWindow { 50 };
     constexpr static inline uint8_t s_nullMoveReduction { 2 };
