0f2c9697ae0ff898a9be8038da1ba615ae71b0df
Hans Binderup <hbinderup94@gmail.com>
2025-03-02 16:40:47 +0100

    bug: add tests for pv table + fix typo bug
    This commit adds tests for PV table.
    Also it helped me find the bug I was looking for. Now the search seems
    good!
    
    Also, the compile_commands.json file was only symlinked if the build
    suceeded. Not very practical :)
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -8,7 +8,7 @@ if [ ! -d "$BUILD_DIR" ]; then
     meson setup "$BUILD_DIR"
 fi
 
+ln -sf "$BUILD_DIR"/compile_commands.json .
+
 # only run the actual executable - we don't care about "tests" here
 meson test --gdb meltdown-chess-engine -C "$BUILD_DIR"
-
-ln -sf "$BUILD_DIR"/compile_commands.json .
