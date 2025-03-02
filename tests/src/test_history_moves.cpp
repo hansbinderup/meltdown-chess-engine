6f3d1f0192c530bf0e36e42bbc80bc722ae009ca
Hans Binderup <hbinderup94@gmail.com>
2025-03-02 16:40:07 +0100

    tests: add history moves test
    This commit adds simple tests for history moves
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

---
+++ tests/src/test_history_moves.cpp
@@ -0,0 +1,76 @@
+#include "src/bit_board.h"
+#include "src/evaluation/history_moves.h"
+#include "src/movement/move_types.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace heuristic;
+using namespace movement;
+
+constexpr inline void setPieceAtSquare(BitBoard& board, Piece type, BoardPosition pos)
+{
+    board.pieces[type] |= 1ULL << pos;
+}
+
+TEST_CASE("HistoryMoves: Updating quiet moves", "[HistoryMoves]")
+{
+    HistoryMoves historyMoves;
+    BitBoard board;
+
+    setPieceAtSquare(board, BlackKnight, C3);
+
+    Move quietMove = Move::create(C3, D5, BlackKnight, false);
+    REQUIRE(historyMoves.get(BlackKnight, D5) == 0);
+
+    historyMoves.update(board, quietMove, 5);
+    REQUIRE(historyMoves.get(BlackKnight, D5) == 5);
+
+    historyMoves.update(board, quietMove, 3);
+    REQUIRE(historyMoves.get(BlackKnight, D5) == 8);
+}
+
+TEST_CASE("HistoryMoves: Ignoring capture moves", "[HistoryMoves]")
+{
+    HistoryMoves historyMoves;
+    BitBoard board;
+
+    setPieceAtSquare(board, BlackPawn, B2);
+
+    Move captureMove = Move::create(B2, C3, BlackPawn, true);
+    Move quietMove = Move::create(B2, B4, BlackPawn, false);
+
+    // only quit moves should be updated
+    historyMoves.update(board, captureMove, 4);
+    REQUIRE(historyMoves.get(BlackPawn, C3) == 0);
+
+    historyMoves.update(board, quietMove, 2);
+    REQUIRE(historyMoves.get(BlackPawn, B4) == 2);
+}
+
+TEST_CASE("HistoryMoves: Handling missing piece cases", "[HistoryMoves]")
+{
+    HistoryMoves historyMoves;
+    BitBoard board;
+
+    Move quietMove = Move::create(E4, G5, BlackBishop, false);
+    historyMoves.update(board, quietMove, 6);
+
+    // Since there is no piece at E4, the history score should remain 0
+    REQUIRE(historyMoves.get(BlackBishop, G5) == 0);
+}
+
+TEST_CASE("HistoryMoves: Reset functionality", "[HistoryMoves]")
+{
+    HistoryMoves historyMoves;
+    BitBoard board;
+
+    setPieceAtSquare(board, BlackQueen, D1);
+    Move quietMove = Move::create(D1, H5, BlackQueen, false);
+
+    historyMoves.update(board, quietMove, 7);
+    REQUIRE(historyMoves.get(BlackQueen, H5) == 7);
+
+    historyMoves.reset();
+    REQUIRE(historyMoves.get(BlackQueen, H5) == 0);
+}
+
