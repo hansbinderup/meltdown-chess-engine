2848f62f6517e9ec75ec29f5109de09a15ad77ec
Hans Binderup <hbinderup94@gmail.com>
2025-03-04 22:34:28 +0100

    tt_hash: add count of hashfull
    This commit adds data on how "full" the hash table is.
    It's a simple counter that will be compared to the full table size.
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/engine/tt_hash.h
+++ src/engine/tt_hash.h
@@ -5,11 +5,7 @@
 #include <array>
 #include <cstdint>
 
-namespace engine::tt {
-
-namespace {
-
-constexpr std::size_t s_ttHashSize { 0x1600000 };
+namespace engine {
 
 enum TtHashFlag : uint8_t {
     TtHashExact = 0,
@@ -24,64 +20,77 @@ struct TtHashEntry {
     int32_t score = 0;
 };
 
-static inline std::array<TtHashEntry, s_ttHashSize> s_ttHashTable;
+class TtHashTable {
+public:
+    static void clearTable()
+    {
+        std::ranges::fill(s_ttHashTable, TtHashEntry {});
+        s_hashCount = 0;
+    }
 
-}
+    static uint16_t getHashFull()
+    {
+        /* should be returned as permill */
+        return std::min((1000 * s_hashCount) / s_ttHashSize, 1000UL);
+    }
 
-// clear TT (hash table)
-inline void clearTable()
-{
-    std::ranges::fill(s_ttHashTable, TtHashEntry {});
-}
+    constexpr static std::optional<int32_t> readHashEntry(uint64_t key, int32_t alpha, int32_t beta, uint8_t depth, uint8_t ply)
+    {
+        auto& entry = s_ttHashTable[key % s_ttHashSize];
 
-constexpr inline std::optional<int32_t> readHashEntry(uint64_t key, int32_t alpha, int32_t beta, uint8_t depth, uint8_t ply)
-{
-    auto& entry = s_ttHashTable[key % s_ttHashSize];
+        if (entry.key != key) {
+            return std::nullopt;
+        }
 
-    if (entry.key != key) {
+        if (entry.depth >= depth) {
+            int32_t score = entry.score;
+
+            /* special case when mating score is found */
+            if (score < -s_mateScore)
+                score += ply;
+            if (score > s_mateScore)
+                score -= ply;
+
+            if (entry.flag == TtHashExact) {
+                return score;
+            }
+
+            if ((entry.flag == TtHashAlpha) && (score <= alpha)) {
+                return alpha;
+            }
+
+            if ((entry.flag == TtHashBeta) && (score >= beta)) {
+                return beta;
+            }
+        }
+
         return std::nullopt;
     }
 
-    if (entry.depth >= depth) {
-        int32_t score = entry.score;
+    constexpr static void writeHashEntry(uint64_t key, int32_t score, uint8_t depth, uint8_t ply, TtHashFlag flag)
+    {
+        auto& entry = s_ttHashTable[key % s_ttHashSize];
 
         /* special case when mating score is found */
         if (score < -s_mateScore)
-            score += ply;
-        if (score > s_mateScore)
             score -= ply;
+        if (score > s_mateScore)
+            score += ply;
 
-        if (entry.flag == TtHashExact) {
-            return score;
-        }
+        entry.key = key;
+        entry.score = score;
+        entry.flag = flag;
+        entry.depth = depth;
 
-        if ((entry.flag == TtHashAlpha) && (score <= alpha)) {
-            return alpha;
-        }
-
-        if ((entry.flag == TtHashBeta) && (score >= beta)) {
-            return beta;
-        }
+        s_hashCount++;
     }
 
-    return std::nullopt;
-}
+private:
+    constexpr static inline std::size_t s_hashTableSizeMb { 64 };
+    constexpr static inline std::size_t s_ttHashSize { (s_hashTableSizeMb * 1024 * 1024) / sizeof(TtHashEntry) };
 
-constexpr inline void writeHashEntry(uint64_t key, int32_t score, uint8_t depth, uint8_t ply, TtHashFlag flag)
-{
-    auto& entry = s_ttHashTable[key % s_ttHashSize];
-
-    /* special case when mating score is found */
-    if (score < -s_mateScore)
-        score -= ply;
-    if (score > s_mateScore)
-        score += ply;
-
-    entry.key = key;
-    entry.score = score;
-    entry.flag = flag;
-    entry.depth = depth;
-}
-
+    static inline std::array<TtHashEntry, s_ttHashSize> s_ttHashTable;
+    static inline uint64_t s_hashCount {};
+};
 }
 
