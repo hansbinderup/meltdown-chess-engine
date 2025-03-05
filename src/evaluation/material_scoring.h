4112e7634b8da7c14c29ad46abcfd15347b6185d
Hans Binderup <hbinderup94@gmail.com>
2025-03-05 23:52:11 +0100

    evaluation: use tapered pesto evaluation
    Instead of our simple eval it was time to implement a more sophisticated
    material evaluation.
    A common evaluation method is called "pesto". Read more here:
    https://www.chessprogramming.org/PeSTO%27s_Evaluation_Function
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/evaluation/material_scoring.h
+++ src/evaluation/material_scoring.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "src/bit_board.h"
+#include "src/evaluation/pesto_tables.h"
 #include "src/evaluation/positioning.h"
 
 #include <cstdint>
@@ -13,49 +14,28 @@
 
 namespace evaluation {
 
-namespace {
-
-/* material score for each piece - lookup based on Piece enum */
-constexpr static inline auto s_pieceScoring = std::to_array<int32_t>({
-    100, /* White Pawn */
-    300, /* White Knight */
-    350, /* White Bishop */
-    500, /* White Rook */
-    1000, /* White Queen */
-    10000, /* White King */
-    -100, /* Black Pawn */
-    -300, /* Black Knight */
-    -350, /* Black Bishop */
-    -500, /* Black Rook */
-    -1000, /* Black Queen */
-    -10000, /* Black King */
-});
-
-}
-
 constexpr int32_t materialScore(const BitBoard& board)
 {
-    int32_t score = 0;
+    /* we first evaluate based on "pesto score"
+     * then we add mobility, double pawn etc. */
+    int32_t score = pesto::evaluate(board);
 
     // Material scoring
     for (const auto piece : magic_enum::enum_values<Piece>()) {
-        score += std::popcount(board.pieces[piece]) * s_pieceScoring[piece];
-
         switch (piece) {
         case WhitePawn:
             score += position::getPawnScore<PlayerWhite>(board, board.pieces[piece]);
             break;
         case WhiteKnight:
-            score += position::getKnightScore<PlayerWhite>(board.pieces[piece]);
             break;
         case WhiteBishop:
-            score += position::getBishopScore<PlayerWhite>(board, board.pieces[piece]);
+            score += position::getBishopScore(board, board.pieces[piece]);
             break;
         case WhiteRook:
             score += position::getRookScore<PlayerWhite>(board, board.pieces[piece]);
             break;
         case WhiteQueen:
-            score += position::getQueenScore<PlayerWhite>(board, board.pieces[piece]);
+            score += position::getQueenScore(board, board.pieces[piece]);
             break;
         case WhiteKing:
             score += position::getKingScore<PlayerWhite>(board, board.pieces[piece]);
@@ -67,16 +47,15 @@ constexpr int32_t materialScore(const BitBoard& board)
             score -= position::getPawnScore<PlayerBlack>(board, board.pieces[piece]);
             break;
         case BlackKnight:
-            score -= position::getKnightScore<PlayerBlack>(board.pieces[piece]);
             break;
         case BlackBishop:
-            score -= position::getBishopScore<PlayerBlack>(board, board.pieces[piece]);
+            score -= position::getBishopScore(board, board.pieces[piece]);
             break;
         case BlackRook:
             score -= position::getRookScore<PlayerBlack>(board, board.pieces[piece]);
             break;
         case BlackQueen:
-            score -= position::getQueenScore<PlayerBlack>(board, board.pieces[piece]);
+            score -= position::getQueenScore(board, board.pieces[piece]);
             break;
         case BlackKing:
             score -= position::getKingScore<PlayerBlack>(board, board.pieces[piece]);
--- src/evaluation/positioning.h
+++ src/evaluation/positioning.h
@@ -11,11 +11,6 @@
 #include "src/movement/kings.h"
 #include "src/movement/rooks.h"
 
-/*
- * Based on the tables from:
- * https://www.chessprogramming.org/Simplified_Evaluation_Function
- */
-
 namespace evaluation {
 
 namespace {
@@ -48,13 +43,11 @@ constexpr static inline int32_t getPawnScore(const BitBoard& board, const uint64
             score += s_isolatedPawnPenalty;
 
         if constexpr (player == PlayerWhite) {
-            score += s_whitePawnTable[pos];
             if ((board.pieces[BlackPawn] & s_whitePassedPawnMaskTable[pos]) == 0) {
                 const uint8_t row = (pos / 8);
                 score += s_passedPawnBonus[row];
             }
         } else {
-            score += s_blackPawnTable[pos];
             if ((board.pieces[WhitePawn] & s_blackPassedPawnMaskTable[pos]) == 0) {
                 const uint8_t row = (pos / 8);
                 score += s_passedPawnBonus[7 - row]; /* invert table for black */
@@ -65,23 +58,6 @@ constexpr static inline int32_t getPawnScore(const BitBoard& board, const uint64
     return score;
 }
 
-template<Player player>
-constexpr static inline int32_t getKnightScore(const uint64_t knights)
-{
-    int32_t score = 0;
-
-    helper::bitIterate(knights, [&score](BoardPosition pos) {
-        if constexpr (player == PlayerWhite) {
-            score += s_whiteKnightTable[pos];
-        } else {
-            score += s_blackKnightTable[pos];
-        }
-    });
-
-    return score;
-}
-
-template<Player player>
 constexpr static inline int32_t getBishopScore(const BitBoard& board, const uint64_t bishops)
 {
     int32_t score = 0;
@@ -89,12 +65,6 @@ constexpr static inline int32_t getBishopScore(const BitBoard& board, const uint
     helper::bitIterate(bishops, [&score, &board](BoardPosition pos) {
         const uint64_t moves = movement::getBishopAttacks(pos, board.occupation[Both]);
         score += std::popcount(moves) * s_bishopMobilityScore;
-
-        if constexpr (player == PlayerWhite) {
-            score += s_whiteBishopTable[pos];
-        } else {
-            score += s_blackBishopTable[pos];
-        }
     });
 
     return score;
@@ -113,12 +83,10 @@ constexpr static inline int32_t getRookScore(const BitBoard& board, const uint64
             score += s_openFileScore;
 
         if constexpr (player == PlayerWhite) {
-            score += s_whiteRookTable[pos];
             if ((whitePawns & s_fileMaskTable[pos]) == 0)
                 score += s_semiOpenFileScore;
 
         } else {
-            score += s_blackRookTable[pos];
             if ((blackPawns & s_isolationMaskTable[pos]) == 0)
                 score += s_semiOpenFileScore;
         }
@@ -127,7 +95,6 @@ constexpr static inline int32_t getRookScore(const BitBoard& board, const uint64
     return score;
 }
 
-template<Player player>
 constexpr static inline int32_t getQueenScore(const BitBoard& board, const uint64_t queens)
 {
     int32_t score = 0;
@@ -138,12 +105,6 @@ constexpr static inline int32_t getQueenScore(const BitBoard& board, const uint6
             | movement::getRookAttacks(pos, board.occupation[Both]);
 
         score += std::popcount(moves) * s_queenMobilityScore;
-
-        if constexpr (player == PlayerWhite) {
-            score += s_whiteQueenTable[pos];
-        } else {
-            score += s_blackQueenTable[pos];
-        }
     });
 
     return score;
@@ -163,14 +124,12 @@ constexpr static inline int32_t getKingScore(const BitBoard& board, const uint64
             score -= s_openFileScore;
 
         if constexpr (player == PlayerWhite) {
-            score += s_whiteKingTable[pos];
             if ((whitePawns & s_fileMaskTable[pos]) == 0)
                 score -= s_semiOpenFileScore;
 
             const uint64_t kingShields = movement::getKingAttacks(pos) & board.occupation[PlayerWhite];
             score += std::popcount(kingShields) * s_kingShieldScore;
         } else {
-            score += s_blackKingTable[pos];
             if ((blackPawns & s_fileMaskTable[pos]) == 0)
                 score -= s_semiOpenFileScore;
 
