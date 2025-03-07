6091fc361c40a2624803d80487795a461897ba50
Hans Binderup <hbinderup94@gmail.com>
2025-03-07 18:30:08 +0100

    version: add version file generation
    This commit adds version file generation.
    We can now specify a version number and pass it as a command line arg to
    the meson build system.
    This number is also built into the engine so we can keep track of the
    version we're running.
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 
 #include "src/uci_handler.h"
+#include "version/version.h"
+
+#include "fmt/base.h"
 
 int main(int argc, char** argv)
 {
     std::ignore = argc;
     std::ignore = argv;
+
+    fmt::print(
+        "============================\n"
+        "          MELTDOWN          \n"
+        "        Chess Engine        \n"
+        "============================\n\n");
+
+    fmt::print("Engine:      Meltdown\n"
+               "Author:      Hans Binderup\n"
+               "Github:      hansbinderup/meltdown-chess-engine\n"
+               "Version:     {}\n"
+               "Build hash:  {}\n"
+               "Build type:  {}\n\n",
+        s_meltdownVersion, s_meltdownBuildHash, s_meltdownBuildType);
 
     UciHandler::run();
 
