9968665187054cefa0db577d8217a0e6951e0cec
Hans Binderup <hbinderup94@gmail.com>
2025-03-11 23:34:54 +0100

    tests: separate tests into individual executables
    Currently the test log is a bit whack; just one big test. Instead make
    them run in parralel and make the test results a bit for explicit :)
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -6,10 +6,13 @@ BUILD_DIR=".build-tests"
 
 
 if [ ! -d "$BUILD_DIR" ]; then
-    meson setup "$BUILD_DIR" -Dunit-tests=true -Db_coverage=true
+    meson setup "$BUILD_DIR" -Dunit-tests=true
 fi
 
 ln -sf "$BUILD_DIR"/compile_commands.json .
 
-# only run the unit tests - we don't want to start the application here
-meson test unit-tests -C "$BUILD_DIR" --print-errorlogs --timeout=10
+# only run the meltdown unit tests - we don't want to start the application here
+meson test meltdown_test* -C "$BUILD_DIR" --print-errorlogs --timeout=10
+
+# example: run gdb for a given test
+# meson test meltdown_test_perft --gdb -C "$BUILD_DIR" --print-errorlogs --timeout=10
