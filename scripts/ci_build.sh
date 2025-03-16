62ff96d2972f8eaa9f359801721394585ba6d8f4
Hans Binderup <hbinderup94@gmail.com>
2025-03-16 17:55:27 +0100

    workflows: make pull_requests build for all archs
    As we're introducing cross compilation it's also important that we keep
    them up to date. Therefore let's build for all of them during our CI.
    
    TODO: do the same for unit-tests?
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

---
+++ scripts/ci_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#!/bin/bash
+
+set -e
+
+BUILD_DIR=".build-ci"
+
+# List of elements that we cross compile.
+# They refer to the .txt files in our 'targets' folder
+ARCHS=(
+    "linux-gcc-x86-64-v2"
+    "linux-gcc-x86-64-v3"
+    "linux-native"
+)
+
+for ARCH in "${ARCHS[@]}"; do
+    echo "Building for $ARCH..."
+
+    # Clean build directory for each architecture
+    rm -rf "$BUILD_DIR"
+    meson setup "$BUILD_DIR" --cross-file targets/$ARCH.txt --wipe --buildtype=release
+    meson compile -C "$BUILD_DIR"
+done
+
