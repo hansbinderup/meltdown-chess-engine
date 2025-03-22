e8d19909b50338a0cdd65ce2852d5d4d2aa12a65
Clara Shepherd <shprd.qlara@gmail.com>
2025-03-22 12:36:46 +0000

    hashing: implement-lock-free-hashing
    * Compress TT hash table's non-key data into a uint64_t
    * Race conditions can only cause a mismatch between these two
    * Compress `Move::data` into uint32_t (low hanging fruit)

--- src/engine/tt_hash_table.h
+++ src/engine/tt_hash_table.h
@@ -4,6 +4,7 @@
 #include "movegen/move_types.h"
 #include <algorithm>
 #include <array>
+#include <atomic>
 #include <cstdint>
 
 namespace engine {
@@ -14,20 +15,87 @@ enum TtHashFlag : uint8_t {
     TtHashBeta = 2,
 };
 
+/*
+ * Non-key members are encoded for compression. From lower bits to higher, entries are:
+ * depth: 7 bits, i.e. 0000 ... 0000 0000 0111 1111
+ * flag: 3 bits, i.e. 0000 ... 0000 0011 1000 0000
+ * score: 17 bits (see board_defs.h)
+ * generation: 8 bits
+ * move: 26 bits (see move_types.h)
+ * */
+
+class TtHashEntryData {
+public:
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshift-count-overflow"
+    explicit TtHashEntryData(uint64_t depth = 0, uint64_t flag = TtHashExact, int64_t score = 0, uint64_t generation = 0, uint64_t move = {})
+        : m_data(
+              depth
+              | flag << s_flagShift
+              | score << s_scoreShift
+              | generation << s_generationShift
+              | move << s_moveShift)
+    {
+    }
+#pragma GCC diagnostic pop
+
+    constexpr inline uint8_t getDepth() const
+    {
+        return m_data & s_depthMask;
+    }
+
+    constexpr inline auto getFlag() const
+    {
+        return static_cast<TtHashFlag>((m_data >> s_flagShift) & s_flagMask);
+    }
+
+    constexpr inline int32_t getScore() const
+    {
+        return (m_data >> s_scoreShift) & s_scoreMask;
+    }
+
+    constexpr inline uint8_t getGeneration() const
+    {
+        return (m_data >> s_generationShift) & s_generationMask;
+    }
+
+    constexpr inline uint32_t getMoveData() const
+    {
+        return (m_data >> s_moveShift) & s_moveMask;
+    }
+
+private:
+    constexpr static inline uint64_t s_depthMask { 0b1111111 };
+
+    constexpr static inline uint64_t s_flagMask { 0b111 };
+    constexpr static inline uint64_t s_flagShift { 7 };
+
+    constexpr static inline uint64_t s_scoreMask { 0b11111111111111111 };
+    constexpr static inline uint64_t s_scoreShift { 10 };
+
+    constexpr static inline uint64_t s_generationMask { 0b11111111 };
+    constexpr static inline uint64_t s_generationShift { 27 };
+
+    constexpr static inline uint64_t s_moveMask { 0b11111111111111111111111111 };
+    constexpr static inline uint64_t s_moveShift { 35 };
+
+    uint64_t m_data;
+};
+
 struct TtHashEntry {
-    uint64_t key = 0;
-    uint8_t depth = 0;
-    TtHashFlag flag = TtHashExact;
-    int32_t score = 0;
-    uint8_t generation = 0;
-    movegen::Move move {};
+    std::atomic<uint64_t> key { 0 };
+    std::atomic<TtHashEntryData> data;
 };
 
 class TtHashTable {
 public:
     static void clear()
     {
-        std::ranges::fill(s_ttHashTable, TtHashEntry {});
+
+        for (auto& entry : s_ttHashTable) {
+            entry.key = 0;
+            entry.data = TtHashEntryData(); // Manually construct
+        }
         s_hashCount = 0;
         s_currentGeneration = 0;
     }
@@ -48,21 +116,24 @@ public:
     {
         auto& entry = s_ttHashTable[key % s_ttHashSize];
 
-        if (entry.key != key) {
+        uint64_t entryKey = entry.key.load(std::memory_order_relaxed);
+        auto entryData = entry.data.load(std::memory_order_relaxed);
+
+        if (entryKey != key) {
             return std::nullopt;
         }
 
         /* Entry is outdated (from a previous generation) */
-        if (entry.generation != s_currentGeneration) {
-            entry.key = 0; // Invalidate the entry
+        if (entryData.getGeneration() != s_currentGeneration) {
+            // Can be racy
             if (s_hashCount > 0) {
                 --s_hashCount;
             }
             return std::nullopt;
         }
 
-        if (entry.depth >= depth) {
-            int32_t score = entry.score;
+        if (entryData.getDepth() >= depth) {
+            int32_t score = entryData.getScore();
 
             /* special case when mating score is found */
             if (score < -s_mateScore)
@@ -70,16 +141,16 @@ public:
             if (score > s_mateScore)
                 score -= ply;
 
-            if (entry.flag == TtHashExact) {
-                return std::make_pair(score, entry.move);
+            if (entryData.getFlag() == TtHashExact) {
+                return std::make_pair(score, movegen::Move(entryData.getMoveData()));
             }
 
-            if ((entry.flag == TtHashAlpha) && (score <= alpha)) {
-                return std::make_pair(alpha, entry.move);
+            if ((entryData.getFlag() == TtHashAlpha) && (score <= alpha)) {
+                return std::make_pair(alpha, movegen::Move(entryData.getMoveData()));
             }
 
-            if ((entry.flag == TtHashBeta) && (score >= beta)) {
-                return std::make_pair(beta, entry.move);
+            if ((entryData.getFlag() == TtHashBeta) && (score >= beta)) {
+                return std::make_pair(beta, movegen::Move(entryData.getMoveData()));
             }
         }
 
@@ -88,24 +159,24 @@ public:
 
     constexpr static void writeEntry(uint64_t key, int32_t score, const movegen::Move& move, uint8_t depth, uint8_t ply, TtHashFlag flag)
     {
-        auto& entry = s_ttHashTable[key % s_ttHashSize];
-
         /* special case when mating score is found */
         if (score < -s_mateScore)
             score -= ply;
         if (score > s_mateScore)
             score += ply;
 
-        if (entry.key == 0) {
+        auto& entry = s_ttHashTable[key % s_ttHashSize];
+
+        if (entry.key.load(std::memory_order_relaxed) == 0) {
             ++s_hashCount;
         }
 
-        entry.key = key;
-        entry.score = score;
-        entry.flag = flag;
-        entry.depth = depth;
-        entry.generation = s_currentGeneration;
-        entry.move = move;
+        TtHashEntryData newData { depth, flag, score, s_currentGeneration, move.getData() };
+
+        // Can be racy
+        static_assert(std::atomic<uint64_t>::is_always_lock_free);
+        entry.key.store(key, std::memory_order_relaxed);
+        entry.data.store(newData, std::memory_order_relaxed);
     }
 
 private:
@@ -118,4 +189,3 @@ private:
     static inline uint8_t s_currentGeneration {};
 };
 }
-
