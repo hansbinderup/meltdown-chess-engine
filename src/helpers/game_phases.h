6b5dc6fc727b7192a97207dff9c21ad129f82fab
Hans Binderup <hbinderup94@gmail.com>
2025-03-23 23:40:45 +0100

    positioning: add helpers to assign table values
    The code became a little convoluted before.
    Now we ensure that each value is updated in a coupled manner.
    
    This increases readability and also minimized future errors when adding
    new scores.
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/helpers/game_phases.h
+++ src/helpers/game_phases.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "evaluation/game_phase.h"
+#include "evaluation/pesto_tables.h"
 #include "magic_enum/magic_enum.hpp"
 
 namespace helper {
@@ -18,6 +19,57 @@ constexpr auto createPhaseArray(Args&&... values)
     static_assert(sizeof...(Args) == magic_enum::enum_count<evaluation::gamephase::Phases>(),
         "Wrong number of phase values");
     return std::array<T, sizeof...(Args)> { std::forward<Args>(values)... };
+}
+
+/* Adds a phase-specific score (midgame and endgame) to the given score object.
+ * `values` must be an array of size 2, where index 0 = midgame, index 1 = endgame.
+ */
+template<typename T>
+constexpr void addPhaseScore(evaluation::gamephase::Score& score, const std::array<T, 2>& values)
+{
+    score.mg += values[evaluation::gamephase::GamePhaseMg];
+    score.eg += values[evaluation::gamephase::GamePhaseEg];
+}
+
+/* Adds a phase-specific score to the given score object, scaled by a float multiplier.
+ * Useful when applying scores based on piece counts, shield counts, mobility, etc.
+ */
+template<typename T>
+constexpr void addPhaseScore(evaluation::gamephase::Score& score, const std::array<T, 2>& values, float multiplier)
+{
+    score.mg += values[evaluation::gamephase::GamePhaseMg] * multiplier;
+    score.eg += values[evaluation::gamephase::GamePhaseEg] * multiplier;
+}
+
+/* Subtracts a phase-specific score from the given score object.
+ * Mirrors addPhaseScore(), but for penalties or score reductions.
+ */
+template<typename T>
+constexpr void subPhaseScore(evaluation::gamephase::Score& score, const std::array<T, 2>& values)
+{
+    score.mg -= values[evaluation::gamephase::GamePhaseMg];
+    score.eg -= values[evaluation::gamephase::GamePhaseEg];
+}
+
+/* Subtracts a phase-specific score from the score object, scaled by a float multiplier.
+ * Useful when reducing score based on factors like lack of development, pawn weaknesses, etc.
+ */
+template<typename T>
+constexpr void subPhaseScore(evaluation::gamephase::Score& score, const std::array<T, 2>& values, float multiplier)
+{
+    score.mg -= values[evaluation::gamephase::GamePhaseMg] * multiplier;
+    score.eg -= values[evaluation::gamephase::GamePhaseEg] * multiplier;
+}
+
+/* Adds midgame and endgame scores for a specific piece at a given board position,
+ * using precomputed PESTO piece-square tables.
+ * Also the material score (game phase dependent) is increased whenever probed
+ */
+constexpr void addPestoPhaseScore(evaluation::gamephase::Score& score, Piece piece, BoardPosition pos)
+{
+    score.mg += evaluation::pesto::s_mgTables[piece][pos];
+    score.eg += evaluation::pesto::s_egTables[piece][pos];
+    score.materialScore += evaluation::gamephase::s_materialPhaseScore[piece];
 }
 
 }
