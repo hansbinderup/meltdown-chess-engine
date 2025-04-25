d76d22ab73097f25a8144da87596f9995cce11b0
Hans Binderup <hbinderup94@gmail.com>
2025-04-25 11:56:42 +0200

    meson: add developer-build option
    This commit adds the option to build a developer build.
    We have different optimization levels. We're already running an
    "optimized" build. BUT, we're still compiling without NDEBUG which has
    quite a runtime penalty.
    
    NDEBUG should only be used for actual releases though. It's quite a
    comfort while developing. There add a setting where we can specify
    developer build settings.
    
    Bench 11349788
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,20 +4,27 @@ set -e
 
 BUILD_DIR=".build"
 RUN=false
+OPTIMIZATION="dev"
+ARGS=()
 
 # Parse flags
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -r) RUN=true ;;
+        --release) OPTIMIZATION="release" ;;
         *) echo "Unknown option: $1" && exit 1 ;;
     esac
     shift
 done
 
+if [[ "$OPTIMIZATION" == "dev" ]]; then
+    ARGS+=("-Ddeveloper-build=true")
+fi
+
 if [ ! -d "$BUILD_DIR" ]; then
     NATIVE_TARGET=$(scripts/get-native-target.sh)
-    echo "Setting up Meson for $NATIVE_TARGET"
-    meson setup "$BUILD_DIR" --cross-file "targets/$NATIVE_TARGET" --buildtype=release
+    echo "Setting up Meson for $NATIVE_TARGET with ARGS: ${ARGS[*]}"
+    meson setup "$BUILD_DIR" --cross-file "targets/$NATIVE_TARGET" --buildtype=release "${ARGS[@]}"
 fi
 
 ln -sf "$BUILD_DIR"/compile_commands.json .
