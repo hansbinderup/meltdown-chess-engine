9a313ccd06e89af750756ccb02ae966f7cf18e3d
Hans Binderup <hbinderup94@gmail.com>
2025-05-08 20:49:53 +0200

    tt_hash_table: improve read and write filtering
    Prior to this commit we would have some issues regarding read and write
    of TT entries.
    
    Let's start with read:
    We would only read an entry if it was from a deeper search than our
    current. This makes sense for our entry test - only return if it's
    actually an exact entry or will cause a cut off.
    However, as we also use the probe for move ordering, we're actually
    losing out on a lot of ttMoves that would otherwise give us an edge.
    
    And write..
    Well.. we had no filter meaning that any result could overwrite any
    entry. So we could have spent forever searching a branch, store it in
    the TT, just to overwrite it with a lesser search. Not very efficient.
    Now we only overwrite the TT entries if there's a better one found.
    
    Bench 8880290
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- tests/src/test_tt_hash_table.cpp
+++ tests/src/test_tt_hash_table.cpp
@@ -20,7 +20,7 @@ TEST_CASE("Transposition Table - Basic Write & Read", "[TT]")
     const auto move = movegen::Move::create(A2, A4, false);
 
     TtHashTable::writeEntry(key, score, move, depth, ply, TtHashExact);
-    auto retrieved = TtHashTable::TtHashTable::probe(key, depth, ply);
+    auto retrieved = TtHashTable::TtHashTable::probe(key);
 
     REQUIRE(retrieved.has_value());
     REQUIRE(retrieved->score == score);
@@ -33,12 +33,12 @@ TEST_CASE("Transposition Table - Edge Case for Positive Score", "[TT]")
     engine::TtHashTable::setSizeMb(16);
 
     uint64_t key = 0x123456789ABCDEF;
-    Score score = s_maxScore;
+    Score score = 2000;
 
     const auto move = movegen::Move::create(A2, A4, false);
 
     TtHashTable::writeEntry(key, score, move, depth, ply, TtHashExact);
-    auto retrieved = TtHashTable::TtHashTable::probe(key, depth, ply);
+    auto retrieved = TtHashTable::TtHashTable::probe(key);
 
     REQUIRE(retrieved.has_value());
     REQUIRE(retrieved->score == score);
@@ -51,12 +51,12 @@ TEST_CASE("Transposition Table - Edge Case for Negative Score", "[TT]")
     engine::TtHashTable::setSizeMb(16);
 
     uint64_t key = 0x123456789ABCDEF;
-    Score score = s_minScore;
+    Score score = -2000;
 
     const auto move = movegen::Move::create(A2, A4, false);
 
     TtHashTable::writeEntry(key, score, move, depth, ply, TtHashExact);
-    auto retrieved = TtHashTable::TtHashTable::probe(key, depth, ply);
+    auto retrieved = TtHashTable::TtHashTable::probe(key);
 
     REQUIRE(retrieved.has_value());
     REQUIRE(retrieved->score == score);
@@ -78,7 +78,7 @@ TEST_CASE("Transposition Table - Depth Overwrite Rule", "[TT]")
     TtHashTable::writeEntry(key, oldScore, move1, 3, ply, TtHashExact);
     TtHashTable::writeEntry(key, newScore, move2, 6, ply, TtHashExact); // Deeper depth
 
-    auto retrieved = TtHashTable::probe(key, 4, ply);
+    auto retrieved = TtHashTable::probe(key);
     REQUIRE(retrieved.has_value());
     REQUIRE(retrieved->score == newScore);
     REQUIRE(retrieved->move == move2);
@@ -98,8 +98,8 @@ TEST_CASE("Transposition Table - Collision Handling", "[TT]")
     TtHashTable::writeEntry(key1, 30, move1, depth, ply, TtHashExact);
     TtHashTable::writeEntry(key2, 99, move2, depth, ply, TtHashExact);
 
-    auto retrieved1 = TtHashTable::probe(key1, depth, ply);
-    auto retrieved2 = TtHashTable::probe(key2, depth, ply);
+    auto retrieved1 = TtHashTable::probe(key1);
+    auto retrieved2 = TtHashTable::probe(key2);
 
     REQUIRE_FALSE(retrieved1.has_value());
 
@@ -109,7 +109,7 @@ TEST_CASE("Transposition Table - Collision Handling", "[TT]")
     REQUIRE(retrieved2->flag == TtHashExact);
 }
 
-TEST_CASE("Transposition Table - Score Clamping for Mating Scores", "[TT]")
+TEST_CASE("Transposition Table - absolute mating Scores", "[TT]")
 {
     engine::TtHashTable::setSizeMb(16);
 
@@ -120,38 +120,12 @@ TEST_CASE("Transposition Table - Score Clamping for Mating Scores", "[TT]")
     const auto move = movegen::Move::create(A2, A4, false);
     TtHashTable::writeEntry(key, nearMate, move, depth, ply, TtHashExact);
 
-    SECTION("From same ply")
-    {
-        auto retrieved = TtHashTable::probe(key, depth, ply);
+    auto retrieved = TtHashTable::probe(key);
 
-        REQUIRE(retrieved.has_value());
-        REQUIRE(retrieved->score == nearMate); // same score
-        REQUIRE(retrieved->move == move);
-        REQUIRE(retrieved->flag == TtHashExact);
-    }
-
-    SECTION("From higher ply")
-    {
-        uint8_t plyOffset = 5;
-        auto retrieved = TtHashTable::probe(key, depth, ply + plyOffset);
-
-        REQUIRE(retrieved.has_value());
-        REQUIRE(retrieved->score == nearMate - plyOffset); // lower score
-        REQUIRE(retrieved->move == move);
-        REQUIRE(retrieved->flag == TtHashExact);
-    }
-
-    SECTION("From lower ply")
-    {
-        uint8_t plyOffset = 1;
-
-        auto retrieved = TtHashTable::probe(key, depth, ply - plyOffset);
-
-        REQUIRE(retrieved.has_value());
-        REQUIRE(retrieved->score == nearMate + plyOffset); // higher score
-        REQUIRE(retrieved->move == move);
-        REQUIRE(retrieved->flag == TtHashExact);
-    }
+    REQUIRE(retrieved.has_value());
+    REQUIRE(retrieved->score == scoreAbsolute(nearMate, ply)); /* score should be absolute */
+    REQUIRE(retrieved->move == move);
+    REQUIRE(retrieved->flag == TtHashExact);
 }
 
 TEST_CASE("Transposition Table - Clear Functionality", "[TT]")
@@ -164,6 +138,72 @@ TEST_CASE("Transposition Table - Clear Functionality", "[TT]")
 
     TtHashTable::clear();
 
-    auto retrieved = TtHashTable::probe(key, depth, ply);
+    auto retrieved = TtHashTable::probe(key);
     REQUIRE_FALSE(retrieved.has_value()); // Should be cleared
+}
+
+TEST_CASE("Transposition Table - Test entry", "[TT]")
+{
+    engine::TtHashTable::setSizeMb(16);
+
+    constexpr Score nearMate = s_mateValue - 10;
+
+    uint64_t key1 = 0xCAFEBABEDEADBEEF;
+    uint64_t key2 = 0xDEADBEEFCAFEBABE;
+    uint64_t key3 = 0xBABECAFEBEEFDEAD;
+
+    const auto move = movegen::Move::create(A2, A4, false);
+    TtHashTable::writeEntry(key1, nearMate, move, depth, ply, TtHashExact);
+    TtHashTable::writeEntry(key2, nearMate, move, depth, ply, TtHashAlpha);
+    TtHashTable::writeEntry(key3, nearMate, move, depth, ply, TtHashBeta);
+
+    auto retrieved = TtHashTable::probe(key1);
+
+    REQUIRE(retrieved.has_value());
+
+    SECTION("Test depth")
+    {
+        /* tests should only succeed if entry has same or deeper depth */
+        REQUIRE_FALSE(testEntry(*retrieved, ply, depth + 1, 0, 0).has_value());
+        REQUIRE_FALSE(testEntry(*retrieved, ply, s_maxSearchDepth, 0, 0).has_value());
+        REQUIRE(testEntry(*retrieved, ply, depth, 0, 0).has_value());
+        REQUIRE(testEntry(*retrieved, ply, depth - 2, 0, 0).has_value());
+        REQUIRE(testEntry(*retrieved, ply, 0, 0, 0).has_value());
+    }
+
+    SECTION("Test flags")
+    {
+        auto retrieved1 = TtHashTable::probe(key1);
+        auto retrieved2 = TtHashTable::probe(key2);
+        auto retrieved3 = TtHashTable::probe(key3);
+
+        REQUIRE(retrieved1.has_value());
+        REQUIRE(retrieved2.has_value());
+        REQUIRE(retrieved3.has_value());
+
+        /* exact should always give a hit - no matter the alpha and beta */
+        REQUIRE(testEntry(*retrieved1, ply, depth, s_maxScore, s_minScore).has_value());
+        REQUIRE(testEntry(*retrieved1, ply, depth, s_minScore, s_maxScore).has_value());
+        REQUIRE(testEntry(*retrieved1, ply, depth, s_minScore, nearMate).has_value());
+        REQUIRE(testEntry(*retrieved1, ply, depth, nearMate, s_maxScore).has_value());
+
+        /* alpha bound should only give a hit if causing a cutoff */
+        REQUIRE(testEntry(*retrieved2, ply, depth, s_maxScore, s_minScore).has_value());
+        REQUIRE_FALSE(testEntry(*retrieved2, ply, depth, s_minScore, nearMate).has_value());
+        REQUIRE(testEntry(*retrieved2, ply, depth, nearMate, s_maxScore).has_value());
+        REQUIRE_FALSE(testEntry(*retrieved2, ply, depth, s_minScore, s_maxScore).has_value());
+
+        /* beta bound should only give a hit if causing a cutoff */
+        REQUIRE(testEntry(*retrieved3, ply, depth, s_maxScore, s_minScore).has_value());
+        REQUIRE(testEntry(*retrieved3, ply, depth, s_minScore, nearMate).has_value());
+        REQUIRE_FALSE(testEntry(*retrieved3, ply, depth, nearMate, s_maxScore).has_value());
+        REQUIRE_FALSE(testEntry(*retrieved3, ply, depth, s_minScore, s_maxScore).has_value());
+    }
+
+    SECTION("Test score")
+    {
+        const auto test = testEntry(*retrieved, ply, depth, s_maxScore, s_minScore);
+        REQUIRE(test.has_value());
+        REQUIRE(*test == nearMate); /* score should be converted from absolute -> relative again */
+    }
 }
