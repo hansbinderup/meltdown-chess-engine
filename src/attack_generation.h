a7637da3129cb0583c384064384703a701f24a53
Hans Binderup <hbinderup94@gmail.com>
2025-05-16 14:52:48 +0200

    attack_gen: add discovered attack generation
    update me
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/attack_generation.h
+++ src/attack_generation.h
@@ -149,4 +149,23 @@ constexpr uint64_t getAllAttacks(const BitBoard& board)
     return attacks;
 }
 
+template<Player player>
+constexpr uint64_t getDiscoveredAttacks(const BitBoard& board, BoardPosition pos)
+{
+    constexpr Player opponent = nextPlayer(player);
+    constexpr Piece opponentRook = opponent == PlayerWhite ? WhiteRook : BlackRook;
+    constexpr Piece opponentBishop = opponent == PlayerWhite ? WhiteBishop : BlackBishop;
+
+    const uint64_t occupancy = board.occupation[Both];
+
+    const uint64_t rookAttacks = movegen::getRookMoves(pos, occupancy);
+    const uint64_t bishopAttacks = movegen::getBishopMoves(pos, occupancy);
+
+    const uint64_t rooks = board.pieces[opponentRook] & ~rookAttacks;
+    const uint64_t bishops = board.pieces[opponentBishop] & ~bishopAttacks;
+
+    return (rooks & movegen::getRookMoves(pos, occupancy & ~rookAttacks))
+        | (bishops & movegen::getBishopMoves(pos, occupancy & ~bishopAttacks));
+}
+
 }
