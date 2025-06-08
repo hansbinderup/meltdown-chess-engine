6fcfeb13709f24b3d08e909314f00ba8e46ff611
Hans Binderup <hbinderup94@gmail.com>
2025-06-08 22:02:30 +0200

    searcher: add insufficient material detection
    The real gains from the beta propagation lies eg. adding insufficient
    material detection.
    
    Making the engine material aware is a very important. And until now it
    had always resulted in a elo loss. No more :)
    
    Bench 2338567
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

--- src/core/bit_board.h
+++ src/core/bit_board.h
@@ -121,6 +121,49 @@ struct BitBoard {
             return occupation[PlayerBlack] == (pieces[BlackPawn] | pieces[BlackKing]);
     }
 
+    inline bool hasInsufficientMaterial() const
+    {
+        /* cheapest check first - always sufficient material if more than 4 pieces */
+        const int totalPieces = std::popcount(occupation[Both]);
+        if (totalPieces > 4) {
+            return false;
+        }
+
+        /* if pawns or majors are on the board, there is always sufficient material */
+        const uint64_t majorsOrPawns = pieces[WhitePawn] | pieces[BlackPawn] | pieces[WhiteRook] | pieces[BlackRook] | pieces[WhiteQueen] | pieces[BlackQueen];
+        if (std::popcount(majorsOrPawns) != 0) {
+            return false;
+        }
+
+        if (totalPieces <= 2) {
+            /* only kings on the board */
+            return true;
+        }
+
+        if (totalPieces == 3) {
+            const uint64_t minors = pieces[WhiteBishop] | pieces[BlackBishop] | pieces[WhiteKnight] | pieces[BlackKnight];
+
+            /* one minor piece + kings is still a draw */
+            return std::popcount(minors) == 1;
+        }
+
+        if (totalPieces == 4) {
+            const uint64_t bishops = pieces[WhiteBishop] | pieces[BlackBishop];
+
+            /* check for exactly one bishop per side */
+            const bool oneWhiteBishop = std::popcount(pieces[WhiteBishop]) == 1;
+
+            /* check if bishops are on opposite color squares */
+            const bool bishopOnLight = std::popcount(s_lightSquares & bishops) == 1;
+            const bool bishopOnDark = std::popcount(s_darkSquares & bishops) == 1;
+
+            return oneWhiteBishop && bishopOnLight && bishopOnDark;
+        }
+
+        /* all other cases have potential for sufficient material */
+        return false;
+    }
+
     std::array<uint64_t, magic_enum::enum_count<Piece>()> pieces {};
     std::array<uint64_t, magic_enum::enum_count<Occupation>()> occupation {};
     std::array<uint64_t, magic_enum::enum_count<Player>()> attacks {};
