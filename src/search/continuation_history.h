9a92cf41aa4e0b3ee0eadfd4d72944050fef9d16
Hans Binderup <hbinderup94@gmail.com>
2025-07-12 21:38:02 +0200

    searcher: add continuation history
    Bench 1357511
    
    Signed-off-by: Hans Binderup <hbinderup94@gmail.com>

---
+++ src/search/continuation_history.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "core/bit_board.h"
+#include "core/board_defs.h"
+#include "movegen/move_types.h"
+
+#include <array>
+
+namespace search {
+
+class ContinuationHistory {
+public:
+    void update(Piece prevPiece, BoardPosition prevPos, movegen::Move currentMove, Score score)
+    {
+        m_continuationTable[prevPiece][prevPos][currentMove.fromPos()][currentMove.toPos()] += score;
+    }
+
+    Score getScore(Piece prevPiece, BoardPosition prevPos, movegen::Move currentMove) const
+    {
+        return m_continuationTable[prevPiece][prevPos][currentMove.fromPos()][currentMove.toPos()];
+    }
+
+private:
+    using ContinuationHistoryEntry = std::array<std::array<std::array<Score, s_amountSquares>, s_amountSquares>, s_amountSquares>;
+    std::array<ContinuationHistoryEntry, magic_enum::enum_count<Piece>()> m_continuationTable {};
+};
+
+}
